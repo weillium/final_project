183e840534676e1e2b1a9759793ac76dbb86342a
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:08:02 +0000

    Direct association between photos and comments

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   has_many   :tags,
              :dependent => :destroy
 
