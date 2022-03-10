f3aba07a7f6b5901d28f5d983e232a66647cd4d9
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:08:44 +0000

    Whitelist columns in Notification for ActiveAdmin

--- app/admin/notifications.rb
+++ app/admin/notifications.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Notification do
 
+ permit_params :creator_id, :title, :body, :attachment
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
