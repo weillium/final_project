6332e00cb99b538996472ee36da991f94b046c88
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:11:42 +0000

    Generate graphiti resources for agenda_items

---
+++ app/resources/agenda_item_resource.rb
@@ -0,0 +1,19 @@
+class AgendaItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :start_time, :datetime
+  attribute :end_time, :datetime
+  attribute :location, :string
+  attribute :activity_name, :string
+  attribute :description, :string
+  attribute :photo, :string
+  attribute :leader_id, :integer
+  attribute :is_cyoa, :boolean
+  attribute :budget_line_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
