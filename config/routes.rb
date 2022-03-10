e1f8962cb35c7ff8c92b36c66b42c60b75886e49
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:08:12 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "user_types#index"
   resources :group_members
   resources :messages
