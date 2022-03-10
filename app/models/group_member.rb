a4257891665767b2953af76d5e5100da38102867
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:08:01 +0000

    Validate presence of group_members.group_id

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -5,6 +5,8 @@ class GroupMember < ApplicationRecord
 
   # Validations
 
+  validates :group_id, :presence => true
+
   validates :user_id, :presence => true
 
   # Scopes
--- spec/models/group_member_spec.rb
+++ spec/models/group_member_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe GroupMember, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:group_id) }
+
     it { should validate_presence_of(:user_id) }
 
     end
