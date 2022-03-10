370d6e9e8be7f34bfb9a76fcf38edea7c6812b59
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:08:30 +0000

    Whitelist columns in BudgetType for ActiveAdmin

--- app/admin/budget_types.rb
+++ app/admin/budget_types.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BudgetType do
 
+ permit_params :type_name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
