1e67583a5b62f1ebc565abd8d5ab806ccd70efb6
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:08:02 +0000

    Direct association between users and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :foreign_key => "commenter_id",
+             :dependent => :destroy
+
   has_many   :photos,
              :foreign_key => "upload_user_id",
              :dependent => :destroy
