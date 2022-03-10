07f6a75115d36e9b41c7b84d1e505fdcfc989776
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 20:08:26 +0000

    Whitelist columns in BudgetLine for ActiveAdmin

--- app/admin/budget_lines.rb
+++ app/admin/budget_lines.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BudgetLine do
 
+ permit_params :budgeted_amount, :actual_amount, :notes, :budget_type_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
